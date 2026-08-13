27c2788b2d144cf98cc46e47991f6214a9962689
Your Full Name <hspatil000@gmail.com>
2026-08-13 14:12:02 +0530

    Controller setup done

---
+++ src/modules/ipl-ms/controllers/owner.controller.js
@@ -0,0 +1,11 @@
+const createOwner = async (req, res) => {};
+
+const getAllOwners = async (req, res) => {};
+
+const getOwnerById = async (req, res) => {};
+
+const updateOwner = async (req, res) => {};
+
+const deleteOwner = async (req, res) => {};
+
+export { createOwner, getAllOwners, getOwnerById, updateOwner, deleteOwner };
---
+++ src/modules/ipl-ms/routes/owner.routes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import * as controller from "../controllers/owner.controller.js";
+//questions -: router.post vs app.post -: app.post means we wanted to keep logic only in a one app we dont wanted to separate anything and for separation we use router.post()
+
+const router = Router();
+
+//Create a new owner
+router.post("/", controller.createOwner);
+
+//get all owners
+router.get("/", controller.getAllOwners);
+
+//get owner by id
+router.get("/:id", controller.getOwnerById);
+
+//update owner
+router.put("/:id", controller.updateOwner);
+
+//delete owner
+router.delete("/:id", controller.deleteOwner);
